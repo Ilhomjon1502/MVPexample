c8ade430d88ae397451ea528be4113aa273ffb8c
dell <IlhomjonIbragimov1502@gmail.com>
2022-02-19 11:04:53 +0500

    Example1

--- app/src/main/java/uz/ilhomjon/mvpexapmle/contact/Contract.kt
+++ app/src/main/java/uz/ilhomjon/mvpexapmle/contact/Contract.kt
@@ -1,13 +1,16 @@
 package uz.ilhomjon.mvpexapmle.contact
 
+//bu interface 3 ta model, view, presenter qismlarni bog'lovchi
 interface Contract {
 
+	//dizayn qismini hozirgi holatda MainActivity boshqaradi
 	interface View {
 		fun showProgress()
 		fun hideProgress()
 		fun setString(string: String?)
 	}
 
+	//malumotlar sqalanadigan joy. Hozirgi holatda Model classi
 	interface Model {
 		interface OnFinishedListener {
 			fun onFinished(string: String?)
@@ -15,6 +18,7 @@ interface Contract {
 		fun getNextCourse(onFinishedListener: OnFinishedListener?)
 	}
 
+	//model va view ni bog'lovchi nazorat qiluvchi, Hozirgi holatda Presenter classi
 	interface Presenter {
 		fun onButtonClick()
 		fun onDestroy()
